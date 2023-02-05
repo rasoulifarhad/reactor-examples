cdb1ac9bbbd547ad11559a85082940a2592a20f9
Farhad rasouli <rasouli.farhad@gmail.com>
2023-02-06 02:16:09 +0330

    first commit

---
+++ basic/basic-reactor/src/main/java/com/farhad/example/reactor/App.java
@@ -0,0 +1,8 @@
+package com.farhad.example.reactor;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+    
+}
---
+++ basic/basic-reactor/src/test/java/com/farhad/example/reactor/AppTest.java
@@ -0,0 +1,33 @@
+package com.farhad.example.reactor;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@Slf4j 
+public class AppTest {
+    
+    @Test
+    public void repeat() {
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        Flux<Integer> repeated = Mono
+                                    .fromCallable(counter::incrementAndGet)
+                                    .repeat(9);
+
+
+        log.info("Repeat: ...");
+
+        StepVerifier
+                .create(repeated.doOnNext(i -> log.info("{}",i)))
+                .expectNext(1,2,3,4,5,6,7,8,9,10)
+                .verifyComplete();
+
+    }
+}
